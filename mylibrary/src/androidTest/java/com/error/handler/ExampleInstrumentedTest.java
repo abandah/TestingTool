a713ca8665447f1f2a4b8932cc550dfcb79c0751
mohammad <m3bndah@gmail.com>
2020-06-01 13:06:48 +0300

    init

--- mylibrary/src/androidTest/java/com/error/handler/ExampleInstrumentedTest.java
+++ mylibrary/src/androidTest/java/com/error/handler/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.mylibrary;
+package com.error.handler;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
--- mylibrary/src/test/java/com/error/handler/ExampleUnitTest.java
+++ mylibrary/src/test/java/com/error/handler/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.willy.pusblishlibrary;
+package com.error.handler;
 
 import org.junit.Test;
 
